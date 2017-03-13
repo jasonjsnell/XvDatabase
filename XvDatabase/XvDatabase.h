596c02440dd8981c751cbc8b4f64418c271423f0
jasonjsnell <snell.jason@gmail>
2017-03-13 07:35:16 -0700

    Initial Commit

---
+++ XvDatabase/XvDatabase.h
@@ -0,0 +1,19 @@
+//
+//  XvDatabase.h
+//  XvDatabase
+//
+//  Created by Jason Snell on 3/13/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvDatabase.
+FOUNDATION_EXPORT double XvDatabaseVersionNumber;
+
+//! Project version string for XvDatabase.
+FOUNDATION_EXPORT const unsigned char XvDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvDatabase/PublicHeader.h>
+
+
